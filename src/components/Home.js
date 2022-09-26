d5c1ca039f1f74ede7b83a411cbdcee529f1cf04
kiboiWanjohi <sammy313.sw@gmail.com>
2022-09-26 13:54:56 +0300

    Add Posts Page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Home.css";
 import Post from "./Post";
+import db from "./Firebase";
 const Home = () => {
+  const [posts, setPosts] = useState([]);
+  // const trialPosts =   {
+  //       username: "wemafisana",
+  //       caption: "Trying Out Astro JS",
+  //       imageURL:
+  //         "https://robohash.org/2600:6c5e:357f:e274:dd4:78bb:b35f:924d.png/",
+  //     },
+  //     {
+  //       username: "yakuza",
+  //       caption: "Destroyer of the earth",
+  //       imageURL:
+  //         "https://robohash.org/2600:6c5e:357f:e274:dd4:78bb:b35f:924d.png/",
+  //     },
+  //     {
+  //       username: "cleeverland",
+  //       caption: "Instagram Sucks",
+  //       imageURL:
+  //         "https://robohash.org/2600:6c5e:357f:e274:dd4:78bb:b35f:924d.png/",
+  //     },
+
+  useEffect(() => {
+    db.collection("posts").onSnapshot((snapshot) => {
+      setPosts(snapshot.docs.map((doc) => doc.data()));
+    });
+  }, []);
   return (
     <div className="home">
       <div className="home-header">
         <p>Be Organic</p>
       </div>
-      <Post />
+      {posts.map(({ id, post }) => (
+        <Post
+          key={id}
+          username={post.username}
+          caption={post.caption}
+          imageURL={post.imageURL}
+        />
+      ))}
     </div>
   );
 };
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,16 +1,27 @@
 import React from "react";
+import "./Post.css";
 
-const Post = () => {
+// Add Material UI
+import Avatar from "@mui/material/Avatar";
+const Post = ({ username, caption, imageURL }) => {
   return (
-    <div>
-      {/* header */}
-      <h3>Username</h3>
+    <div className="post">
+      <div className="post-header">
+        {/* header */}
+
+        <Avatar
+          className="post-avatar"
+          alt={username}
+          src="https://robohash.org/2600:6c5e:357f:e274:dd4:78bb:b35f:924d.png"
+        ></Avatar>
+        <h3>{username}</h3>
+      </div>
       {/* image */}
-      <img
-        src="https://robohash.org/2600:6c5e:357f:e274:dd4:78bb:b35f:924d.png"
-        alt=""
-      />
+      <img className="post-img" src={imageURL} alt="" />
       {/* username and caption  */}
+      <h4 className="post-text">
+        <strong>{username}:</strong> {caption}
+      </h4>
     </div>
   );
 };
